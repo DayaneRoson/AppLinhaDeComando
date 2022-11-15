65ee2edd3a9131e2f9e08563c20639576a608d67
Dayane Roson <dayane.roson@gmail.com>
2022-11-15 19:38:21 -0300

    App - Linha de comando commit inicial

---
+++ App/app.go
@@ -0,0 +1,13 @@
+package app
+
+import "github.com/urfave/cli"
+
+//Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+func Gerar() *cli.App {
+
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca por IPs e Nomes de Servidor na Internet"
+
+	return app
+}
