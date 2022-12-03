cbccca0e3794c52052b814d4c9a52b6d39916906
Dayane Roson <dayane.roson@gmail.com>
2022-12-02 22:37:42 -0300

    Busca por IP implementado

--- App/app.go
+++ App/app.go
@@ -1,13 +1,48 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-//Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser executada
 func Gerar() *cli.App {
 
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca por IPs e Nomes de Servidor na Internet"
+	app.Author = "Day"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca por IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "bibliaonline.com.br/acf",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+
+	host := c.String("host")
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
 }
